7363d3a5104e71ba792c6be8471a77ae2c51742c
Anton <104672550+Samusev-Anton@users.noreply.github.com>
2022-10-28 08:57:13 +0200

    fetch api

--- src/js/themovieApi.js
+++ src/js/themovieApi.js
@@ -8,11 +8,12 @@
 // eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyN2E3NmYwODY5YWZkNTllYWZjY2VmN2Q2ZDk4NmMyMCIsInN1YiI6IjYzNWFjODY2MTUxMWFhMDA3ZTlkMjA0OSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.u4oo3dqnEhbq4-mU65_77HUxgSjunDdlP98tEZ8BJ8w
 
 const KEY='27a76f0869afd59eafccef7d6d986c20';
+const URL='https://api.themoviedb.org/3'
 
  export async function apiHomePage() {
     try {
       const responce = await fetch(
-        `https://api.themoviedb.org/3/trending/movie/week?api_key=${KEY}`
+        `${URL}/trending/movie/week?api_key=${KEY}`
       );
       const data = await responce.json();
       console.log(data);
@@ -28,7 +29,22 @@ const KEY='27a76f0869afd59eafccef7d6d986c20';
  export async function apiHomeSearch() {
     try {
       const responce = await fetch(
-        `https://api.themoviedb.org/3/trending/movie/week?api_key=${KEY}`
+        `${URL}/search/movie?api_key=${KEY}&query=world&language=en-US&page=1&include_adult=false`
+      );
+      const data = await responce.json();
+      console.log(data);
+      return data;
+    } catch (Error) {
+    //   Notiflix.Notify.failure(
+    //     'Sorry, there are no images matching your search query. Please try again.'
+    //   );
+    }
+ }
+
+ export async function apiModalDetails() {
+    try {
+      const responce = await fetch(
+        `${URL}/movie/507086?api_key=${KEY}&language=en-US`
       );
       const data = await responce.json();
       console.log(data);
