87c0b86e950327d0040eb49e4328a457012eb06c
Anton <104930896+Zmiievskyi@users.noreply.github.com>
2022-11-07 15:48:28 +0200

    update library theme

--- src/js/library-page.js
+++ src/js/library-page.js
@@ -5,11 +5,14 @@ import { dataToYear } from './data/data-revize';
 const libraryGallery = document.querySelector('.library-gallery');
 const btnQueue = document.querySelector('.js-queue');
 const btnWatched = document.querySelector('.js-watched');
+const libraryTitle = document.querySelector('.library-text');
+let movieTitle;
 
 btnQueue.addEventListener('click', onClickQueue);
 btnWatched.addEventListener('click', onClickWatched);
 
 function onClickQueue() {
+  btnWatched.classList.remove('button--accent');
   markupLibrary('queue');
 }
 function onClickWatched() {
@@ -17,24 +20,24 @@ function onClickWatched() {
 }
 let listFilms;
 markupLibrary('watched');
-
+movieTitle = document.querySelector('.film-card__title');
 function markupLibrary(key) {
-  libraryGallery.innerHTML = '';
-
-  try {
-    listFilms = localStorageAPI.load(key);
-    listFilms.map(obj => {
-      obj.genres.splice(1);
-      obj.vote_average = Number(obj.vote_average.toFixed(1));
-      return obj;
-    });
-    const normalListFilms = dataToYear(listFilms);
-    console.log(normalListFilms);
-    libraryGallery.innerHTML = markupLibraryPage(normalListFilms);
     
-  } catch (error) {
-    console.log(error);
-  }
-}
+  libraryGallery.innerHTML = '';
+  listFilms = localStorageAPI.load(key);
 
+  if (listFilms.length >= 1) {
+    libraryTitle.classList.add('is-hidden');
+  }
+
+  listFilms.map(obj => {
+    obj.genres.splice(1);
+    obj.vote_average = Number(obj.vote_average.toFixed(1));
+    return obj;
+  });
+  const normalListFilms = dataToYear(listFilms);
+  console.log(normalListFilms);
+  libraryGallery.innerHTML = markupLibraryPage(normalListFilms);
+  
+}
 
