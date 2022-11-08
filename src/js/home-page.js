62c75a2807fac9f7fdba055e867e7f412de49f9e
vsC0dex <max071010@gmail.com>
2022-11-08 19:39:48 +0200

    Update home-page.js(Other)

--- src/js/home-page.js
+++ src/js/home-page.js
@@ -1,3 +1,19 @@
+// import markupHomePage from './templates/markupHomePage.hbs';
+// import { apiHomePage } from './themovieApi';
+// import { refs } from './refs';
+// import { getGenres, dataRevize } from './data/data-revize';
+
+// apiHomePage().then(data => {
+//   const allGenres = getGenres();
+//   //   console.log(allGenres);
+//   const films = data.results;
+//   //   console.log(films);
+//   const normalFilmData = dataRevize(films, allGenres);
+//   console.log(normalFilmData);
+//   refs.homeGallery.innerHTML = markupHomePage(normalFilmData);
+// });
+
+
 import markupHomePage from './templates/markupHomePage.hbs';
 import { apiHomePage } from './themovieApi';
 import { refs } from './refs';
@@ -9,6 +25,11 @@ apiHomePage().then(data => {
   const films = data.results;
   //   console.log(films);
   const normalFilmData = dataRevize(films, allGenres);
+  normalFilmData.forEach(element => {
+  if (element.genre_ids.length > 3) {
+    element.genres.splice(2, 2, {name: 'Other'})
+  }
+});
   console.log(normalFilmData);
   refs.homeGallery.innerHTML = markupHomePage(normalFilmData);
 });
