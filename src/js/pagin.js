36d46413e318024a072def8e40ef923bd380c04c
Kateryna Ostrynska <kateriinag@gmail.com>
2022-11-08 22:15:17 +0200

    Update pagin.js

--- src/js/pagin.js
+++ src/js/pagin.js
@@ -1,13 +1,9 @@
-import { totalPages } from './themovieApi';
-// console.log(totalPages);
 import Pagination from 'tui-pagination';
-// import { apiHomePage } from "./themovieApi";
+// import 'tui-pagination/dist/tui-pagination.css';
 import { refs } from './refs';
-import { renderTrendingMovies } from './home-page';
 import { getGenres, dataRevize } from './data/data-revize';
 import markupHomePage from './templates/markupHomePage.hbs';
-import apiHomePage  from "./api/api-movie";
-
+import { apiHomeSearch } from "./search-films";
 import {
   API_KEY,
   BASE_URL,
@@ -16,15 +12,19 @@ import {
   ID_URL,
 } from './api/api-parts';
 
-
-// ----- POPULAR -----
 const container = document.getElementById('pagination');
+
+
+// --------------- POPULAR ---------------
+//pagination options for trending movies request 
 const optionsTrending = {
-  totalItems:  20000,
+  totalItems: 20000,
   itemsPerPage: 20,
-  visiblePages: 5,
+  visibleResults: 5,
   page: 1,
   centerAlign: true,
+  firstItemClassName: 'tui-first-child',
+  lastItemClassName: 'tui-last-child',
   template: {
     page: '<a href="#" class="tui-page-btn  pagination_button">{{page}}</a>',
     currentPage:
@@ -43,19 +43,12 @@ const optionsTrending = {
       '</a>',
   },
 };
+
 const pagination = new Pagination(container, optionsTrending);
 
-export const paginationPage = pagination.getCurrentPage();
-console.log(paginationPage);
-
-pagination.on('afterMove', function (event) {
-  
-  // console.log("currentPage", event.page);
-
+pagination.on('afterMove', event => {
   apiHomePagePagin(event.page).then(data => {
-    const allGenres = getGenres();
-    const films = data.results;
-    const normalFilmData = dataRevize(films, allGenres);
+    const normalFilmData = dataRevize(data.results, getGenres());
     refs.homeGallery.innerHTML = markupHomePage(normalFilmData);
   });
 
@@ -68,100 +61,65 @@ export async function apiHomePagePagin(page) {
       `${TREND_URL}?api_key=${API_KEY}&page=${page}`
     );
     const data = await responce.json();
-    totalPages = data.total_pages;
     return data;
-  } catch (Error) {
-      Notiflix.Notify.failure(
-        'Sorry, there are no images matching your search query. Please try again.'
-      );
+  } catch (error) {
+    console.log(error);
   }
 }
 
 
-// ----- FILTER -----
-export function paginationFilter(genre, year, sort, page) {
-  const optionsFilter = {
-    totalItems: JSON.parse(localStorage.getItem('moviesData')),
-    itemsPerPage: 20,
-    visiblePages: 5,
-    page: 1,
-    centerAlign: true,
-    template: {
-      page: '<a href="#" class="tui-page-btn">{{page}}</a>',
-      currentPage:
-        '<strong class="tui-page-btn tui-is-selected">{{page}}</strong>',
-      moveButton:
-        '<a href="#" class="tui-page-btn tui-{{type}}">' +
-        '<span class="tui-ico-{{type}}">{{type}}</span>' +
-        '</a>',
-      disabledMoveButton:
-        '<span class="tui-page-btn tui-is-disabled tui-{{type}}">' +
-        '<span class="tui-ico-{{type}}">{{type}}</span>' +
-        '</span>',
-      moreButton:
-        '<a href="#" class="tui-page-btn tui-{{type}}-is-ellip">' +
-        '<span>⋅⋅⋅</span>' +
-        '</a>',
-    },
-  };
+//pagination options for movies request 
+const options = {
+  totalItems: 21,
+  itemsPerPage: 20,
+  visibleResults: 5,
+  page: 1,
+  centerAlign: true,
+  firstItemClassName: 'tui-first-child',
+  lastItemClassName: 'tui-last-child',
+  template: {
+    page: '<a href="#" class="tui-page-btn  pagination_button">{{page}}</a>',
+    currentPage:
+      '<strong class="tui-page-btn tui-is-selected">{{page}}</strong>',
+    moveButton:
+    '<a href="#" class="tui-page-btn tui-{{type}}">' +
+      '<span class="tui-ico-{{type}}">{{type}}</span>' +
+      '</a>',
+    disabledMoveButton:
+      '<span class="tui-page-btn tui-is-disabled tui-{{type}}">' +
+      '<span class="tui-ico-{{type}}">{{type}}</span>' +
+      '</span>',
+    moreButton:
+      '<a href="#" class="tui-page-btn tui-{{type}}-is-ellip">' +
+      '<span>⋅⋅⋅</span>' +
+      '</a>',
+  },
+};
 
-  const pagination = new Pagination(container, optionsFilter);
-  pagination.on('afterMove', async function (event) {
+// --------------- FILTER ---------------
+export function paginationFilter(genre, year, sort, page) {
+  
+  const pagination = new Pagination(container, options);
+
+  pagination.on('afterMove', async event => {
     getSearchForm(genre, year, event.page, sort)
       .then(data => {
         markupSearchPage(data.results);
       })
       .catch(error => console.log(error));
+
     window.scrollTo({ top: 0, behavior: 'smooth' });
   });
 }
 
-
-// ----- SEARCH -----
+// --------------- SEARCH ---------------
 export function paginationSearch(inputData) {
-  const optionsSearch = {
-    totalItems: JSON.parse(localStorage.getItem('moviesData')),
-    itemsPerPage: 20,
-    visiblePages: 5,
-    page: 1,
-    centerAlign: true,
-    firstItemClassName: 'tui-first-child',
-    lastItemClassName: 'tui-last-child',
-    template: {
-      page: '<a href="#" class="tui-page-btn">{{page}}</a>',
-      currentPage:
-        '<strong class="tui-page-btn tui-is-selected">{{page}}</strong>',
-      moveButton:
-        '<a href="#" class="tui-page-btn tui-{{type}}">' +
-        '<span class="tui-ico-{{type}}">{{type}}</span>' +
-        '</a>',
-      disabledMoveButton:
-        '<span class="tui-page-btn tui-is-disabled tui-{{type}}">' +
-        '<span class="tui-ico-{{type}}">{{type}}</span>' +
-        '</span>',
-      moreButton:
-        '<a href="#" class="tui-page-btn tui-{{type}}-is-ellip">' +
-        '<span>⋅⋅⋅</span>' +
-        '</a>',
-    },
-  };
-
-  const pagination = new Pagination(container, optionsSearch);
-  // pagination.movePageTo(1);
-  pagination.on('afterMove', async function (event) {
-    resetGallery();
-
+  const pagination = new Pagination(container, options);
+  pagination.on('afterMove', async event => {
     apiHomeSearch(inputData, event.page);
 
     window.scrollTo({ top: 0, behavior: 'smooth' });
   });
 }
 
-
 export default pagination;
-
-
-
-function resetGallery() {
-  refs.homeGallery.innerHTML = '';
-}
