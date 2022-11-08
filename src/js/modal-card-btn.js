47bd8adbca4ffd7987ae28fc02a0b5d029a61b81
Weselka <weselyna.shvets@gmail.com>
2022-11-08 13:52:48 +0200

    add function filter
    add filter in function addWatchedBtn & addQueueBtn
    add function errorNotify

--- src/js/modal-card-btn.js
+++ src/js/modal-card-btn.js
@@ -5,38 +5,61 @@ let storageWatched = [];
 let storageQueue = [];
 
 export function addWatchedBtn(key, obj) {
-  // console.dir(obj.id);
   let arrWatched = localStorage.getItem(key);
   if (storageWatched.includes(obj)) {
-    Notify.failure('we have alredy added that movie');
+    errorNotify();
     return;
   } else if (arrWatched === null) {
     storageWatched.push(obj);
-    // console.dir(storageWatched[0].id);
     localStorageAPI.save(key, storageWatched);
   } else {
     storageWatched = JSON.parse(arrWatched);
     // console.log(storageWatched);
-    storageWatched.push(obj);
-    localStorageAPI.save(key, storageWatched);
+    let uniqueId = storageWatched.filter(({ id }) => id === obj.id);
+    // console.dir(Object.keys(uniqueId));
+    // console.log(uniqueId.length);
+    if (uniqueId.length >= 1) {
+      // console.log(uniqueId.length >= 1);
+      errorNotify();
+      localStorage.removeItem(key);
+      localStorageAPI.save(key, storageWatched);
+    } else {
+      // console.log(uniqueId === []);
+      storageWatched.push(obj);
+      localStorage.removeItem(key);
+      localStorageAPI.save(key, storageWatched);
+    }
   }
 }
 
 export function addQueueBtn(key, obj) {
-  let arrQueue = localStorage.getItem(key);
-  // console.log(arrQueue);
+  let arrQueue = localStorage.getItem(key); // console.dir(obj.id);
   if (storageQueue.includes(obj)) {
     Notify.failure('we have alredy added that movie');
     return;
   } else if (arrQueue === null) {
-    // console.dir(arrQueue);
-    storageQueue.push(obj);
-    // console.log(storageQueue);
+    storageQueue.push(obj); // console.dir(storageQueue[0].id);
     localStorageAPI.save(key, storageQueue);
   } else {
     storageQueue = JSON.parse(arrQueue);
     // console.log(storageQueue);
-    storageQueue.push(obj);
-    localStorageAPI.save(key, storageQueue);
+    let uniqueId = storageQueue.filter(({ id }) => id === obj.id);
+    // console.dir(Object.keys(uniqueId));
+    // console.log(uniqueId.length);
+    if (uniqueId.length >= 1) {
+      // console.log(uniqueId.length >= 1);
+      Notify.failure('we have alredy added that movie');
+      localStorage.removeItem(key);
+      localStorageAPI.save(key, storageQueue);
+    } else {
+      // console.log(uniqueId === []);
+      storageQueue.push(obj);
+      localStorage.removeItem(key);
+      localStorageAPI.save(key, storageQueue);
+    }
   }
+}
+
+function errorNotify() {
+  Notify.failure('we have alredy added that movie');
 }
